9f9b340abcced4de64faa1ddef6fa1e25c046d1c
Benjamin Oldén <berntsson90@hotmail.com>
2023-12-04 14:40:31 +0100

    Add Index and details for product pages for customer view.

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,18 +10,26 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
-        public IActionResult Privacy()
+		public IActionResult Details(int productId)
+		{
+			Product product = _unitOfWork.Product.Get(p=>p.Id == productId, includeProperties: "Category");
+			return View(product);
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
