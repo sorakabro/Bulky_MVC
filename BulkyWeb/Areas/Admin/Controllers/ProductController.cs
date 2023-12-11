209332445e4240f884ac5af0393008ab74e517cc
Benjamin Oldén <berntsson90@hotmail.com>
2023-12-11 16:08:40 +0100

    add image upload to product

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -61,31 +61,31 @@ namespace BulkyWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-				/* string wwwRootPath = _webHostEnvironment.WebRootPath;
-				 if (file != null)
-				 {
-					 string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-					 string productPath = Path.Combine(wwwRootPath, @"images\product");
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
 
-					 if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
-					 {
-						 var oldImagePath = Path.Combine(wwwRootPath,productVM.Product.ImageUrl.TrimStart('\\'));
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath,productVM.Product.ImageUrl.TrimStart('\\'));
 
-						 if (System.IO.File.Exists(oldImagePath))
-						 {
-							 System.IO.File.Delete(oldImagePath);
-						 }
-					 }
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
 
-					 using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
-					 {
-						 file.CopyTo(fileStream);
-					 }
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
 
-					 productVM.Product.ImageUrl = @"\images\product\" + fileName;
-				 } */
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
 
-				if (productVM.Product.Id == 0)
+                if (productVM.Product.Id == 0)
                 {
 					_unitOfWork.Product.Add(productVM.Product);
 				} else
