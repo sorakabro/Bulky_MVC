c18cf5d76d33abd7956ab106109703bc1ac864dc
Benjamin Oldén <berntsson90@hotmail.com>
2023-12-11 17:18:45 +0100

    add validate never for ImageUrl

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -46,6 +46,7 @@ namespace Bulky.Models
         [ForeignKey("CategoryId")]
         [ValidateNever]
         public Category Category { get; set; }
-        public string ImageUrl { get; set; }
+		[ValidateNever]
+		public string ImageUrl { get; set; }
     }
 }
